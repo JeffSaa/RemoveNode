c20fbbae7d0b9d2c69c23d9047a971512142eb55
Jeff <jeffreysaa@outlook.com>
2017-03-15 10:57:39 -0500

    Main menu ready

--- src/removenode/RemoveNode.java
+++ src/removenode/RemoveNode.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package removenode;
+
+import java.util.Scanner;
 
 /**
  *
- * @author jeffr
+ * @author jeff
  */
 public class RemoveNode {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int op = -1;
+        while(op != 0){
+            System.out.println("Presiona 1) para agregar un nodo.");
+            System.out.println("Presiona 2) para buscar y eliminar un nodo.");
+            System.out.println("Presiona 0) para salir.");
+            System.out.print("Que quieres hacer? ");
+            op = Integer.parseInt(sc.nextLine());
+            switch(op){
+                case 1:
+                    addNode();
+                    break;
+                case 2:
+                    delNode();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta, intentalo de nuevo.");
+                    break;
+            }
+        }
+    }
+    
+    private static void addNode(){
+        
+    }
+    
+    private static void delNode(){
+        
     }
     
 }
